3026136f39a57e9acf22a16ac2b83071994f9c54
Harshit Pushkarna <harshitpushkarna@192.168.1.5>
2026-06-26 16:03:13 +0530

    feat: implement authentication controllers

---
+++ server/src/controllers/auth.controller.js
@@ -0,0 +1,28 @@
+const AsyncHandler = require("../utils/AsyncHandler");
+const ApiResponse = require("../utils/ApiResponse");
+const AuthService = require("../services/user.service")
+
+const register = AsyncHandler(async (req,res)=>{
+
+    const {user,accessToken,refreshToken} = await AuthService.register(req.body);
+
+    res.cookie("accessToken",accessToken,{httpOnly:true,secure:true,maxAge:60*60*1000})
+    res.cookie("refreshToken",refreshToken,{httpOnly:true,secure:true,maxAge:60*60*24*7*1000})
+
+    res.status(201).json(new ApiResponse(201, user,"User registered successfully"))
+})
+
+const login = AsyncHandler(async (req,res)=>{
+
+    const {user,accessToken,refreshToken} = await AuthService.login(req.body);
+
+    res.cookie("accessToken",accessToken,{httpOnly:true,secure: process.env.NODE_ENV === "production",sameSite: process.env.NODE_ENV === "production" ? "strict" : "lax",maxAge:60*60*1000})
+    res.cookie("refreshToken",refreshToken,{httpOnly:true,secure: process.env.NODE_ENV === "production",sameSite: process.env.NODE_ENV === "production" ? "strict" : "lax",maxAge:60*60*24*7*1000})
+
+    res.status(200).json(new ApiResponse(200,user,"User logged in successfully"))
+})
+
+module.exports = {
+    register,
+    login
+}   
