1167792d410c464e81b22748ca5330191e90c624
Harshit Pushkarna <harshitpushkarna@192.168.1.5>
2026-06-26 19:16:35 +0530

    refactor: enhance ApiError with validation errors and stack trace

--- server/src/utils/ApiError.js
+++ server/src/utils/ApiError.js
@@ -1,9 +1,11 @@
 class ApiError extends Error{
-    constructor(statusCode,message){
+    constructor(statusCode,message,errors=[]){
         super(message)
         this.success = false
         this.statusCode = statusCode
-        this.message = message
+        this.errors = errors
+
+        Error.captureStackTrace(this,this.constructor)
     }
 }   
 
