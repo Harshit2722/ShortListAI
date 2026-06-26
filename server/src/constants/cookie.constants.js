368005e28a800344a1cd09d526290e280fca166b
Harshit Pushkarna <harshitpushkarna@192.168.1.5>
2026-06-26 20:00:23 +0530

    feat: add secure cookie configuration

---
+++ server/src/constants/cookie.constants.js
@@ -0,0 +1,19 @@
+const ACCESS_COOKIE_OPTIONS = {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+    maxAge: 15 * 60 * 1000
+}
+
+
+const REFRESH_COOKIE_OPTIONS = {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+    maxAge: 7 * 24 * 60 * 60 * 1000
+}
+
+module.exports = {
+    ACCESS_COOKIE_OPTIONS,
+    REFRESH_COOKIE_OPTIONS
+}
