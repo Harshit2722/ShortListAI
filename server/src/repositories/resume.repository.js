4fcf18599dff4e667f0d2df129aba8d37e67394f
Harshit Pushkarna <harshitpushkarna@192.168.1.3>
2026-07-02 20:32:23 +0530

    fix: clean up resume repository delete method and remove invalid select calls

--- server/src/repositories/resume.repository.js
+++ server/src/repositories/resume.repository.js
@@ -3,29 +3,24 @@ const ResumeSubmission = require("../models/resumeSubmission.model");
 class ResumeSubmissionRepository{
 
     async createResume(resumeData){
-        return await ResumeSubmission.create({
-            ...resumeData
-        }).select("-_v")
+        return await ResumeSubmission.create(resumeData)
     }
 
     async findResumeWithHash(jobId,hash){
         return await ResumeSubmission.findOne({
             job:jobId,
             fileHash:hash
-        }).select("-_v")
+        }).select("-__v")
     }
 
     async findResumeById(id){
-        return await ResumeSubmission.findById(id).select("-_v")
+        return await ResumeSubmission.findById(id).select("-__v")
     }
 
     async getResumesByJob(jobId){
-        return await ResumeSubmission.find({job:jobId}).select("-_v")
+        return await ResumeSubmission.find({job:jobId}).select("-__v")
     }
 
-    async deleteResume(id){
-        return await ResumeSubmission.findByIdAndDelete(id);
-    }
 }
 
 module.exports = new ResumeSubmissionRepository();
