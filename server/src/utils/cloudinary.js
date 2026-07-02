1c744633462c557ca35785b1bae2d517cba717fa
Harshit Pushkarna <harshitpushkarna@192.168.1.7>
2026-07-02 17:30:18 +0530

    feat: add cloudinary upload utility

---
+++ server/src/utils/cloudinary.js
@@ -0,0 +1,29 @@
+const cloudinary = require("../config/cloudinary");
+const streamifier = require("streamifier");
+
+const uploadResume = (buffer) => {
+    return new Promise((resolve, reject) => {
+        const uploadStream = cloudinary.uploader.upload_stream(
+            {
+                resource_type: "raw",
+                folder: process.env.CLOUDINARY_RESUME_FOLDER
+            },
+            (error, result) => {
+                if (error) {
+                    return reject(error);
+                }
+
+                resolve({
+                    url: result.secure_url,
+                    publicId: result.public_id
+                });
+            }
+        );
+
+        streamifier.createReadStream(buffer).pipe(uploadStream);
+    });
+};
+
+module.exports = {
+    uploadResume
+};
