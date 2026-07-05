0787bda65ec3ebeade3f95d02fa5b0688b5a270f
Harshit Pushkarna <harshitpushkarna@192.168.1.7>
2026-07-05 19:15:35 +0530

    refactor: forward seniority context to AI service

--- server/src/services/ai/index.js
+++ server/src/services/ai/index.js
@@ -2,9 +2,9 @@ const {buildResumeAnalysisPrompt} = require("./prompt");
 const {generateCompletion} = require("./groq");
 const {validateResponse} = require("./validator");
 
-const analyzeResume = async ({resumeText,jobDescription,jobTitle,requiredSkills,requiredExperience}) => {
+const analyzeResume = async ({resumeText,jobDescription,jobTitle,requiredSkills,requiredExperience,seniority}) => {
 
-    const prompt = buildResumeAnalysisPrompt({resumeText,jobDescription,jobTitle,requiredSkills,requiredExperience});
+    const prompt = buildResumeAnalysisPrompt({resumeText,jobDescription,jobTitle,requiredSkills,requiredExperience,seniority});
 
     const response = await generateCompletion(prompt);
 
