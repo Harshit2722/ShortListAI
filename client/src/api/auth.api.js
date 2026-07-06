1a8937996e7bcdd9925bd385747ff77b082038e8
Harshit Pushkarna <harshitpushkarna@192.168.1.3>
2026-07-07 02:00:17 +0530

    chore: setup axios and auth API client

---
+++ client/src/api/auth.api.js
@@ -0,0 +1,21 @@
+import api from './axios'
+
+export const register = async(userData)=>{
+    const { data } = await api.post("/auth/register", userData)
+    return data;
+}
+
+export const login = async (userData) => {
+    const { data } = await api.post("/auth/login", userData)
+    return data;
+}
+
+export const logout = async () => {
+    const { data } = await api.post("/auth/logout")
+    return data;
+}
+
+export const refreshAccessToken = async (refreshToken) => {
+    const { data } = await api.post("/auth/refresh", { refreshToken })
+    return data;
+}
